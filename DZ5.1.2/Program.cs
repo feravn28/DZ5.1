8c374df2c10eeded8eeb4f44092475518495a49a
Raupov Firdaus <feravn28@gmail.com>
2021-11-01 10:39:01 +0300

    Добавьте файлы проекта.

---
+++ DZ5.1.2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ5._1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("VVod");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[,] table = new int[n, n];
+            for (int t = 0; t < n; t++)
+            {
+                int k = 1;
+                for (int i = 0; i < n; i++)
+                {
+
+                    table[i, t] = k;
+                    if (k == 1) k = 0;
+                    else k = 1;
+                    Console.Write(k+"\t");
+
+
+
+                }
+                Console.WriteLine();
+
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DZ5.1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.Write("");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("");
+            int number3 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("");
+            int number4 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("");
+            int number5 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("");
+            int number6 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("");
+            int number7 = Convert.ToInt32(Console.ReadLine());
+            int[] n = new int[7];
+            int a = 0;
+            n[0] = number1;
+            n[1] = number2;
+            n[2] = number3;
+            n[3] = number4;
+            n[4] = number5;
+            n[5] = number6;
+            n[6] = number7;
+            for (int i = 0; i < 7; i++) a = a + n[i];
+            a = a / 7;
+            Console.WriteLine(" Среднее арифметическое " + a);
+            Console.ReadKey();
+        }
+    }
+}
